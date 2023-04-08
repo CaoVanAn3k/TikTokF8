38edf3fc6aa04010324d2230df3472279e33a25f
Cao Van An <you@example.com>
2023-04-08 11:08:05 +0700

    pussh code

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,26 @@
+import classNames from 'classnames/bind';
+import styles from './Accountitem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+function Accountitem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/b0acb550ed07d896defefa2f49909d66~c5_300x300.webp?x-expires=1681048800&x-signature=PhB%2FCDPA6VP2FsYNXOr9Cus8y9Y%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default Accountitem;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h2>Homes pages</h2>;
+}
+
+export default Home;
